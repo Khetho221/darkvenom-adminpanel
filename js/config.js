8ffd9c2ec6a3d9a44035dff5bfa629d1d42f58f5
DARK MASTER <mkansikhethoashmond@gmail.com>
2025-12-23 04:16:01 +0200

    Delete js/config.js

--- js/config.js
+++
@@ -1,6 +0,0 @@
-export const OWNER_USERNAME = "DARKVENOM";
-
-export const ADMINS = [
-  { user: "DARKVENOM", pass: "DARKVENOM-AGENT042", role: "OWNER" },
-  { user: "PlayboyRSA", pass: "rsa123", role: "VIP" }
-];
