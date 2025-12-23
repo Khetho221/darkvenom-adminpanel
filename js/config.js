76ed1c3c906366e834871a4c11ef802bf61b9cea
DARK MASTER <mkansikhethoashmond@gmail.com>
2025-12-23 02:59:14 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+export const OWNER_USERNAME = "DARKVENOM";
+
+export const ADMINS = [
+  { user: "DARKVENOM", pass: "DARKVENOM-AGENT042", role: "OWNER" },
+  { user: "PlayboyRSA", pass: "rsa123", role: "VIP" }
+];
